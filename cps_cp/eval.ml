de306020e62fefdadd536f1a283cbe2b44216e9a
本田華歩 <kaho_honda@hondahanafunoMacBook-Pro.local>
2020-12-29 19:10:19 +0900

    removed

--- cps_cp/eval.ml
+++ cps_cp/eval.ml
@@ -35,28 +35,28 @@ let rec term t env  = match t with
     Number (n) -> fun k -> fun t -> k (TNum (n)) t
   | Plus (t1, t2) ->
     let v = Gensym.f "v" in
-    let t'' = Gensym.f "t''" in
-    let env2 = Env.add (Env.add env "v" (TVar (v))) "t''" (TVar (t'')) in
-    fun k -> fun t -> (term t1 env2)
-      (fun v1 -> fun t1 -> (term t2 env2)
-          (fun v2 -> fun t2  -> TApp(TPlus(v1, v2),  TApp(TFun(v, TFun(t'', k (TVar (v)) (TVar (t'')))), t2))) t1)  t
+    fun k -> fun t -> (term t1 env)
+      (fun v1 -> fun t1 -> (term t2 env)
+          (fun v2 -> fun t2  ->
+             TLet(v, TPlus(v1, v2), k (TVar(v)) t2)) t1) t
+  (* TApp(TPlus(v1, v2),  TApp(TFun(v, TFun(t'', k (TVar (v)) (TVar (t'')))), t2))) t1)  t *)
+      
   | Var (x) ->   fun k -> fun t -> k (Env.get env x) t
       
   | Fun (x, e) ->
     let x = Gensym.f "x" in
     let k' = Gensym.f "k'" in
     let t' = Gensym.f "t'" in
-    let env2 = Env.add (Env.add (Env.add env "x" (TVar (x))) "k'" (TVar (k'))) "t'" (TVar (t')) in
-    fun k -> fun t -> k (TFun (x, TFun(k', TFun(t', (term e env2)
-                                      (fun v -> fun t'' -> TApp((TVar (k')), TApp(v, t''))) (TVar (t')))))) t
+    fun k -> fun t -> k (TFun (x, TFun(k', TFun(t', (term e env)
+                                      (fun v -> fun t'' -> TApp(TApp((TVar (k')),v), t'')) (TVar (t')))))) t
         
   | App (t1, t2) ->
     let v = Gensym.f "v" in
-    let t'' = Gensym.f "t''" in
-    let env2 = Env.add (Env.add env "v" (TVar (v))) "t''" (TVar (t'')) in
-    fun k -> fun t -> (term t1 env2)
-      (fun v1 -> fun t1 -> (term t2 env2)
-          (fun v2 -> fun t2  -> TApp(v1, TApp(v2, TApp(TFun(v, TFun(t'', k (TVar (v)) (TVar (t'')))), t2)))) t1)  t
+    fun k -> fun t -> (term t1 env)
+      (fun v1 -> fun t1 -> (term t2 env)
+          (fun v2 -> fun t2  ->
+             TLet(v, TApp(v1, v2), k (TVar(v)) t2)) t1) t
+
 
 (* k=TFun(v, TFun(t'', TApp(k, TApp((TVar (v)), (TVar (t'')))))) *)
   | Control (c, e) ->
@@ -65,19 +65,16 @@ let rec term t env  = match t with
     let t' = Gensym.f "t'" in
     let v = Gensym.f "v" in
     let t'' = Gensym.f "t''" in
-    let env2 = Env.add (Env.add (Env.add (Env.add (Env.add env "x" (TVar (x))) "k'" (TVar (k')))
-                                   "t'" (TVar (t'))) "v" (TVar (v))) "t''" (TVar (t'')) in
     fun k -> fun t ->
       TLet(x, TFun(v, TFun(k', TFun(t',
                                     TLet(t'', TAppend(t,TCons((TVar(k')),(TVar(t')))), k (TVar(v)) (TVar(t'')))))),
-           (term e (Env.add env2 c (TVar(x))) idk idt))
+           (term e (Env.add env c (TVar(x))) idk idt))
       
  
   | Prompt (e) ->
     let v = Gensym.f "v" in
-    let env2 = Env.add env "v" (TVar(v)) in
     fun k -> fun t ->
-      TLet(v, (term e env2) idk idt, k (TVar(v)) t)       
+      TLet(v, (term e env) idk idt, k (TVar(v)) t)       
                     
 
 
