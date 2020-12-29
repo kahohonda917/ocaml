df5bb7a0189881cd16404b160629ea95a48c913b
本田華歩 <kaho_honda@hondahanafunoMacBook-Pro.local>
2020-12-30 01:48:27 +0900

    modified

--- cps_cp/eval.ml
+++ cps_cp/eval.ml
@@ -24,15 +24,17 @@ let rec term t env  = match t with
     let x = Gensym.f "x" in
     let k' = Gensym.f "k'" in
     let t' = Gensym.f "t'" in
-    fun k -> fun t -> k (TFun (x, TFun(k', TFun(t', (term e env)
-                                      (fun v -> fun t'' -> TApp(TApp((TVar (k')), v), t'')) (TVar (t')))))) t
+    let env2 = Env.add env "x" (TVar(x)) in
+    fun k -> fun t -> k (TFun (x, TFun(k', TFun(t', (term e env2)
+                                                  (fun v -> fun t'' -> TApp(TApp((TVar (k')), v), t'')) (TVar (t')))))) t
         
   | App (t1, t2) ->
     let v = Gensym.f "v" in
+    let t'' = Gensym.f "t''" in
     fun k -> fun t -> (term t1 env)
       (fun v1 -> fun t1 -> (term t2 env)
           (fun v2 -> fun t2  ->
-             TLet(v, TApp(v1, v2), k (TVar(v)) t2)) t1) t
+             TApp (TApp ((TApp(v1, v2)), TFun(v, TFun(t'', k (TVar(v)) (TVar(t''))))), t2)) t1) t
 
 
 
